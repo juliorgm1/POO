b6466cd06ef77c6f42adfac51aaae09b0244f87b
juliorgm1 <julio.developer1@gmail.com>
2020-09-15 17:16:03 -0300

    "Alteração na classe ProgramaArray"

---
+++ src/funcionarios/Empresa.java
@@ -0,0 +1,20 @@
+package funcionarios;
+
+public class Empresa {
+    private String nome;
+    private String cnpj;
+    private Funcionario [] listaFuncionarios;
+    private int ultimoFuncionario = 0;
+
+    public Empresa(String nome){
+        this.nome = nome;
+        this.listaFuncionarios = new Funcionario[10];
+    }
+
+    public void add(Funcionario funcionario){
+        if (this.listaFuncionarios.length > ultimoFuncionario) {
+            this.listaFuncionarios[ultimoFuncionario] = funcionario;
+            ultimoFuncionario++;
+        }else {System.out.println("Não há vagas");}
+    }
+}
---
+++ src/funcionarios/TestaEmpresa.java
@@ -0,0 +1,48 @@
+package funcionarios;
+
+public class TestaEmpresa {
+    public static void main(String[] args) {
+
+        Empresa senac = new Empresa("SENAC");
+
+        Funcionario novoFuncionario = new Funcionario("José","TI",3000,"12/12/2011","963258");
+
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+        senac.add(novoFuncionario);
+
+    }
+
+
+
+
+
+
+
+
+
+    private static Funcionario[] createFuncionarios(){
+        Funcionario []listaFuncionario = new Funcionario[10];
+        listaFuncionario[0] = new Funcionario("José","TI",3000,"12/12/2011","963258");
+        listaFuncionario[1] = new Funcionario("Maria","Compras",2000,"12/12/2011","963258");
+        listaFuncionario[2] = new Funcionario("Ivo","Compras",3000,"12/12/2011","963258");
+        listaFuncionario[3] = new Funcionario("Joaquim","TI",2500,"12/12/2011","963258");
+        listaFuncionario[4] = new Funcionario("Rosana","Compras",4500,"12/12/2011","963258");
+        listaFuncionario[5] = new Funcionario("Guilherme","Marketing",3500,"12/12/2011","963258");
+        listaFuncionario[6] = new Funcionario("Ana","Compras",2745,"12/12/2011","963258");
+        listaFuncionario[7] = new Funcionario("Patrícia","Compras",5600,"12/12/2011","963258");
+        listaFuncionario[8] = new Funcionario("Nilce","Marketing",5400,"12/12/2011","963258");
+        listaFuncionario[9] = new Funcionario("Adão","Marketing",3600,"12/12/2011","963258");
+
+        return  listaFuncionario;
+    }
+}
